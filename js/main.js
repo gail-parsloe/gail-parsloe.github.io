299b3ad118e5b4b019164e890498eb1f14756c6c
Gail Parsloe <gail.parsloe@umass.edu>
2020-04-05 11:37:51 -0400

    removing reference to serviceworker

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-window.onload = () => {
-  'use strict';
- console.log('inside main.js');
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-             .register('./sw.js');
-  }
+// window.onload = () => {
+//   'use strict';
+//  console.log('inside main.js');
+//   if ('serviceWorker' in navigator) {
+//     navigator.serviceWorker
+//              .register('./sw.js');
+//   }
 
-}
+// }
