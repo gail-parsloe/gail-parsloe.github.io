75687d3a960b676dc1fd6d7700238dd181d6a78f
Gail Parsloe <gail.parsloe@umass.edu>
2020-04-05 11:38:59 -0400

    reviving main.js without sw

--- js/main.js
+++ js/main.js
@@ -1,9 +1,4 @@
-// window.onload = () => {
-//   'use strict';
-//  console.log('inside main.js');
-//   if ('serviceWorker' in navigator) {
-//     navigator.serviceWorker
-//              .register('./sw.js');
-//   }
-
-// }
+window.onload = () => {
+  'use strict';
+ console.log('inside main.js take 2');
+}
