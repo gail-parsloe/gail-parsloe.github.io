fe527a3b5e213e06d91003b8ff5a3d543f9215a8
Gail Parsloe <gail.parsloe@umass.edu>
2020-04-05 08:54:18 -0400

    debug

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 window.onload = () => {
   'use strict';
-
+ console.log('inside main.js');
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker
              .register('./sw.js');
   }
+
 }
